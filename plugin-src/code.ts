337fba290c5cad30fa6bfc2caea256cc7349c062
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-03-10 09:02:30 +0100

    Make UI functional

--- plugin-src/code.ts
+++ plugin-src/code.ts
@@ -1,96 +1,159 @@
 import { clone } from "./clone";
 
+async function getCollectionName() {
+  const collections = await figma.variables.getLocalVariableCollectionsAsync();
+
+  const collection = collections[0].name;
+  console.log(collection);
+  return collection;
+}
+
 async function main() {
   try {
     const collections =
       await figma.variables.getLocalVariableCollectionsAsync();
+    const collectionNames = collections.map((collection) => collection.name);
+    figma.ui.postMessage({
+      type: "COLLECTIONS",
+      collections: collections.map((collection) => ({
+        id: collection.id,
+        name: collection.name,
+      })),
+    });
+    console.log("collections on plugin: ", collections);
+    let targetCollection: VariableCollection;
+    let targetVariables = [];
+    let nodes;
 
-    const targetCollection = collections[0];
+    // Move these interface/type definitions to the top level
+    interface PaintableProperties {
+      fills: Paint[] | PluginAPI["mixed"];
+      strokes: Paint[] | PluginAPI["mixed"];
+      [key: string]: any;
+    }
 
-    const selectionColors = figma.getSelectionColors();
-    if (selectionColors) {
-      const paintsArray = selectionColors.paints;
-      const targetVariables = [];
+    type PaintableNode = SceneNode & PaintableProperties;
 
-      for (const paint of paintsArray) {
-        if (paint.type === "SOLID" && paint.boundVariables?.color) {
-          console.log(paint);
-          const sourceVariableId = paint.boundVariables.color.id;
-          const sourceVariable =
-            await figma.variables.getVariableByIdAsync(sourceVariableId);
-          const sourceCollectionId = sourceVariable?.variableCollectionId;
-          const sourceCollection =
-            await figma.variables.getVariableCollectionByIdAsync(
-              sourceCollectionId!,
+    interface VariableMapping {
+      sourceVariableId: string;
+      targetVariable: Variable;
+    }
+
+    function swapVariables(
+      node: PaintableNode,
+      property: "fills" | "strokes",
+      targetArray: VariableMapping[],
+    ) {
+      const paints = node[property];
+      if (!paints || paints === figma.midex || paints.length === 0) {
+        return;
+      }
+      if (paints !== figma.mixed && paints.length > 0) {
+        const propsCopy = clone(node[property]);
+        if (!propsCopy || !propsCopy[0]) {
+          return;
+        }
+        for (const { sourceVariableId, targetVariable } of targetArray) {
+          if (propsCopy[0].boundVariables?.color?.id === sourceVariableId) {
+            propsCopy[0] = figma.variables.setBoundVariableForPaint(
+              propsCopy[0],
+              "color",
+              targetVariable,
             );
-
-          const sourceVariableIndex = sourceCollection?.variableIds.findIndex(
-            (id) => {
-              return id === sourceVariableId;
-            },
-          );
-
-          const targetCollectionIds = targetCollection.variableIds;
-          const targetVariableId = targetCollectionIds[sourceVariableIndex!];
-          const targetVariable =
-            await figma.variables.getVariableByIdAsync(targetVariableId);
-
-          if (targetVariable) {
-            targetVariables.push({ sourceVariableId, targetVariable });
           }
         }
-      }
-
-      interface PaintableProperties {
-        fills: Paint[] | PluginAPI["mixed"];
-        strokes: Paint[] | PluginAPI["mixed"];
-        [key: string]: any;
-      }
-
-      type PaintableNode = SceneNode & PaintableProperties;
-
-      interface VariableMapping {
-        sourceVariableId: string;
-        targetVariable: Variable;
-      }
-
-      function swapVariables(
-        node: PaintableNode,
-        property: "fills" | "strokes",
-        targetArray: VariableMapping[],
-      ) {
-        const paints = node[property];
-        if (paints !== figma.mixed && paints.length > 0) {
-          const propsCopy = clone(node[property]);
-          for (const { sourceVariableId, targetVariable } of targetArray) {
-            if (propsCopy[0].boundVariables?.color?.id === sourceVariableId) {
-              propsCopy[0] = figma.variables.setBoundVariableForPaint(
-                propsCopy[0],
-                "color",
-                targetVariable,
-              );
-            }
-          }
-          node[property] = propsCopy;
-        }
-      }
-      const selectedNode = figma.currentPage.selection[0] as ChildrenMixin &
-        SceneNode;
-      const nodes = selectedNode
-        .findAll()
-        .filter(
-          (node): node is PaintableNode => "fills" in node && "strokes" in node,
-        );
-      for (let node of nodes) {
-        swapVariables(node, "fills", targetVariables);
-        swapVariables(node, "strokes", targetVariables);
+        node[property] = propsCopy;
       }
     }
-    figma.closePlugin();
+
+    function performSwap(nodes: PaintableNode[]) {
+      console.log("TEST");
+      for (let node of nodes) {
+        // console.log(node);
+        swapVariables(node, "fills", targetVariables as VariableMapping[]);
+      }
+    }
+
+    async function processVariables() {
+      const selectionColors = figma.getSelectionColors();
+      if (selectionColors) {
+        const paintsArray = selectionColors.paints;
+        // const targetVariables = [];
+
+        for (const paint of paintsArray) {
+          if (paint.type === "SOLID" && paint.boundVariables?.color) {
+            // console.log(paint);
+            const sourceVariableId = paint.boundVariables.color.id;
+            const sourceVariable =
+              await figma.variables.getVariableByIdAsync(sourceVariableId);
+            const sourceCollectionId = sourceVariable?.variableCollectionId;
+            const sourceCollection =
+              await figma.variables.getVariableCollectionByIdAsync(
+                sourceCollectionId!,
+              );
+            if (sourceCollection) {
+              figma.ui.postMessage({
+                type: "CURRENT_COLLECTION_NAME",
+                name: sourceCollection.name,
+              });
+            }
+
+            const sourceVariableIndex = sourceCollection?.variableIds.findIndex(
+              (id) => {
+                return id === sourceVariableId;
+              },
+            );
+
+            if (targetCollection !== undefined) {
+              const targetCollectionIds = targetCollection?.variableIds;
+              const targetVariableId =
+                targetCollectionIds[sourceVariableIndex!];
+              const targetVariable =
+                await figma.variables.getVariableByIdAsync(targetVariableId);
+              if (targetVariable) {
+                targetVariables.push({ sourceVariableId, targetVariable });
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // Move message handler here, at main function level
+    figma.ui.onmessage = async (message) => {
+      if (message.type === "TARGET_COLLECTION") {
+        const targetCollectionId = message.value?.id;
+        targetCollection =
+          await figma.variables.getVariableCollectionByIdAsync(
+            targetCollectionId,
+          );
+        if (targetCollection) {
+          await processVariables();
+        }
+      }
+      if (message.type === "SWAP") {
+        // console.log("click");
+        const selectedNode = figma.currentPage.selection[0] as ChildrenMixin &
+          SceneNode;
+        nodes = selectedNode
+          .findAll()
+          .filter(
+            (node): node is PaintableNode =>
+              "fills" in node && "strokes" in node,
+          );
+
+        if (targetCollection && targetVariables.length > 0) {
+          performSwap(nodes);
+        }
+      }
+    };
   } catch (error) {
     console.error(error);
   }
 }
 
 figma.showUI(__html__, { themeColors: true, width: 240, height: 152 });
-// main();
+main();
+
+// const collection = getCollectionName();
+// figma.ui.postMessage(collection);
--- ui-src/App.tsx
+++ ui-src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, forwardRef } from "react";
+import React, { useRef, useState, forwardRef, useEffect } from "react";
 import "./App.css";
 import { Label, Select } from "radix-ui";
 import {
@@ -9,7 +9,71 @@ import {
 } from "@radix-ui/react-icons";
 import classnames from "classnames";
 
+interface Collection {
+  name: string;
+  id: string;
+}
+
 function App() {
+  const [collections, setCollections] = useState<Collection[]>([]);
+  const [currentCollectionName, setCurrentCollectionName] = useState("");
+  const [targetCollection, setTargetCollection] = useState<Collection | null>(
+    null,
+  );
+
+  function findNextAvailable(currentName: string) {
+    console.log("Finding next available. Current:", currentName);
+    console.log("Available collections:", collections);
+
+    // Instead of find, let's try filter first to see all available options
+    const available = collections.filter((c) => c.name !== currentName);
+    console.log("Available options:", available);
+
+    return available[0]; // or you could pick a random one
+  }
+
+  useEffect(() => {
+    if (collections.length > 0 && !targetCollection) {
+      const firstAvailable = collections.find(
+        (collection) => collection.name !== currentCollectionName,
+      );
+      if (firstAvailable) {
+        setTargetCollection({
+          name: firstAvailable.name,
+          id: firstAvailable.id,
+        });
+      }
+    }
+  }, [collections]);
+
+  useEffect(() => {
+    onmessage = (event) => {
+      const message = event.data.pluginMessage;
+      if (message.type === "COLLECTIONS") {
+        setCollections(message.collections);
+      } else if (message.type === "CURRENT_COLLECTION_NAME") {
+        if (message.name !== currentCollectionName) {
+          setCurrentCollectionName(message.name);
+        }
+      }
+    };
+  }, [currentCollectionName]);
+
+  useEffect(() => {
+    parent.postMessage(
+      { pluginMessage: { type: "TARGET_COLLECTION", value: targetCollection } },
+      "*",
+    );
+  }, [targetCollection]);
+
+  function handleClick() {
+    if (targetCollection) {
+      parent.postMessage({ pluginMessage: { type: "SWAP" } }, "*");
+      setCurrentCollectionName(targetCollection.name);
+      setTargetCollection(null);
+    }
+  }
+
   const SelectItem = forwardRef(
     ({ children, className, ...props }, forwardedRef) => {
       return (
@@ -35,7 +99,7 @@ function App() {
         </Label.Root>
         <Select.Root disabled>
           <Select.Trigger className="SelectTrigger" aria-label="Food">
-            <Select.Value placeholder="Gruvbox" />
+            <Select.Value placeholder={currentCollectionName} />
             {/* <Select.Icon className="SelectIcon">
               <CaretDownIcon />
             </Select.Icon> */}
@@ -46,8 +110,18 @@ function App() {
         <Label.Root className="LabelRoot" htmlFor="firstName">
           To
         </Label.Root>
-        <Select.Root>
-          <Select.Trigger className="SelectTrigger" aria-label="Food">
+        <Select.Root
+          value={targetCollection?.name ?? ""}
+          onValueChange={(event) => {
+            const selected = collections.find(
+              (collection) => collection.name === event,
+            );
+            if (selected) {
+              setTargetCollection({ name: selected.name, id: selected.id });
+            }
+          }}
+        >
+          <Select.Trigger className="SelectTrigger" aria-label="Collections">
             <Select.Value placeholder="Select collection" />
             <Select.Icon className="SelectIcon">
               <CaretDownIcon />
@@ -60,11 +134,14 @@ function App() {
               </Select.ScrollUpButton>
               <Select.Viewport className="SelectViewport">
                 <Select.Group>
-                  <SelectItem value="one">One</SelectItem>
-                  <SelectItem value="ayu">Ayu</SelectItem>
-                  <SelectItem value="moegi">Moegi</SelectItem>
-                  <SelectItem value="github">Github</SelectItem>
-                  <SelectItem value="tokyo-night">Tokyo Night</SelectItem>
+                  {collections.map(
+                    ({ name }) =>
+                      name !== currentCollectionName && (
+                        <SelectItem key={name} value={name}>
+                          {name}
+                        </SelectItem>
+                      ),
+                  )}
                 </Select.Group>
               </Select.Viewport>
               <Select.ScrollDownButton className="SelectScrollButton">
@@ -75,7 +152,9 @@ function App() {
         </Select.Root>
       </div>
       <footer className="Footer">
-        <button className="Button">Swap collection</button>
+        <button onClick={handleClick} className="Button">
+          Swap collection
+        </button>
       </footer>
     </main>
   );
